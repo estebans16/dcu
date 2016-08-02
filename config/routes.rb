b7b3befb0d9aa46de2e198b882b81d071cc8a06b
Nicolas <nicolasmalcorra@gmail.com>
2016-08-02 15:46:46 -0300

    Agregando derechos

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
 
   get 'contacto' => 'contacto#vista_contacto'
 
+  get 'derechos' => 'derechos#vista_derechos'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
